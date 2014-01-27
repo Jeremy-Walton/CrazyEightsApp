999d56bc42f239e887f47838e41ecdcc42ceb4a7
Jeremy Walton <jthelegoman@gmail.com>
2014-01-27 10:10:00 -0500

    Initial Commit

---
+++ CrazyEightsApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CrazyEightsApp
+//
+//  Created by Jeremy on 1/27/14.
+//  Copyright (c) 2014 Jeremy-Walton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CrazyEightsApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CrazyEightsApp
+//
+//  Created by Jeremy on 1/27/14.
+//  Copyright (c) 2014 Jeremy-Walton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
